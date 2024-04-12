d13f017b0d6d9e63781588b133df03f6862ee27b
ashishpathak2 <pathak1420@gmail.com>
2024-04-12 13:59:49 +0530

    code changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
         element: <Customize />
       },
       {
-        path: "adminpanel",
+        path: "admin",
         element: <Admins />
       },
       {
